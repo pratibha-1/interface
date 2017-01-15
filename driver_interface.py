ea6fe310cfd244382433f7d81ab3807cdfee2902
pratibha-1 <pratibha.2902s@gmail.com>
2017-01-15 12:14:47 +0530

    Update driver_interface.py

--- driver_interface.py
+++ driver_interface.py
@@ -1,7 +1,7 @@
 class Driver_Passenger_Matching:
     def matching(Passenger_Source,Passenger_Destination,timestamp,Passenger_id):
           getEstimateFare(Passenger_Source,Passenger_Destination)
-          Pass driver detail/unavailability of ride to Handling Driver and Passenger request Api
+          Pass driver_id/unavailability of ride to Handling Driver and Passenger request Api
              
 
 
